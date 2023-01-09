bf8b9e02a8c1b2192dee1019862638b66e3a2746
Sébastien Bonami <sbonami@ferlab.bio>
2023-01-09 16:25:10 -0500

    fix: add needed libvirt pool

--- image/image.tf
+++ image/image.tf
@@ -1,4 +1,11 @@
+resource "libvirt_pool" "default" {
+  name = "default"
+  type = "dir"
+  path = "/var/lib/virt/pools/default"
+}
+
 resource "libvirt_volume" "ubuntu_focal_2022_12_13" {
   name   = "ubuntu-focal-2022-12-13"
+  pool   = libvirt_pool.default.name
   source = "http://cloud-images.ubuntu.com/releases/focal/release-20221213/ubuntu-20.04-server-cloudimg-amd64.img"
 }

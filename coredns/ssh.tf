251a4f920f0917c0d4c1e238d970688e222b9805
Eric Vallee <eric_vallee@webificservices.com>
2023-01-28 09:23:58 -0500

    feat: INFRA-252 Added ssh keys export in shared directory

--- coredns/ssh.tf
+++ coredns/ssh.tf
@@ -2,3 +2,9 @@ resource "tls_private_key" "admin_ssh" {
   algorithm   = "RSA"
   rsa_bits    = 4096
 }
+
+resource "local_file" "admin_ssh" {
+  content         = tls_private_key.admin_ssh.private_key_openssh
+  file_permission = "0600"
+  filename        = "${path.module}/../shared/coredns_ssh_key"
+}

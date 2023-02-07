38ea1aea6b31deab93b6e6c60603be43c61a1f86
Eric Vallee <eric_vallee@webificservices.com>
2023-02-06 23:47:31 -0500

    feat: INFRA-243 Added postgres parameters and networking

---
+++ netaddr/postgres.tf
@@ -0,0 +1,23 @@
+resource "netaddr_address_ipv4" "postgres" {
+    count      = 3
+    range_id   = netaddr_range_ipv4.ip.id
+    name       = "ferlab-postgres-${count.index + 1}"
+    depends_on = [netaddr_address_ipv4.coredns]
+}
+
+resource "netaddr_address_mac" "postgres" {
+    count    = 3
+    range_id = netaddr_range_mac.mac.id
+    name     = "ferlab-postgres-${count.index + 1}"
+}
+
+resource "netaddr_address_ipv4" "postgres_lb" {
+    range_id   = netaddr_range_ipv4.ip.id
+    name       = "ferlab-postgres-lb-1"
+    depends_on = [netaddr_address_ipv4.coredns]
+}
+
+resource "netaddr_address_mac" "postgres_lb" {
+    range_id = netaddr_range_mac.mac.id
+    name     = "ferlab-postgres-lb-1"
+}

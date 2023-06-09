620af2a9e756da1791848423625a4bd96b066b72
Eric Vallee <eric_vallee@webificservices.com>
2023-06-09 14:40:44 -0400

    feat: INFRA-399 Updated networking for automations server orchestration

--- netaddr/prometheus.tf
+++
@@ -1,9 +0,0 @@
-/*resource "netaddr_address_ipv4" "prometheus" {
-    range_id = netaddr_range_ipv4.ip.id
-    name     = "ferlab-prometheus-1"
-}
-
-resource "netaddr_address_mac" "prometheus" {
-    range_id = netaddr_range_mac.mac.id
-    name     = "ferlab-prometheus-1"
-}*/

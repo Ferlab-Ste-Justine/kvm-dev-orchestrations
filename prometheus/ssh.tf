3849d983431e38dfa5c3ad6c6061dddd19c4560b
Sébastien Bonami <sbonami@ferlab.bio>
2024-12-05 16:23:18 -0500

    feat: INFRA-817 fix ssh key local file for prometheus

--- prometheus/ssh.tf
+++ prometheus/ssh.tf
@@ -6,5 +6,5 @@ resource "tls_private_key" "admin_ssh" {
 resource "local_file" "admin_ssh" {
   content         = tls_private_key.admin_ssh.private_key_openssh
   file_permission = "0600"
-  filename        = "${path.module}/../shared/postgres_ssh_key"
+  filename        = "${path.module}/../shared/prometheus_ssh_key"
 }

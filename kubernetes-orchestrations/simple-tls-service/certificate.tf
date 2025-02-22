c6f79190cb1f2f77621b7a1a4a9feac83679bfdd
Eric Vallee <eric_vallee@webificservices.com>
2025-02-22 09:33:14 -0500

    feat: INFRA-975 Added domain to simple service for proxy in front. Added client cert.

--- kubernetes-orchestrations/simple-tls-service/certificate.tf
+++ kubernetes-orchestrations/simple-tls-service/certificate.tf
@@ -11,7 +11,8 @@ resource "tls_private_key" "simple_service" {
 resource "tls_cert_request" "simple_service" {
   private_key_pem = tls_private_key.simple_service.private_key_pem
   dns_names = [
-    "simple-service.k8.ferlab.lan"
+    "simple-service.k8.ferlab.lan",
+    "simple-service.ferlab.lan"
   ]
   ip_addresses = [
     data.netaddr_address_ipv4.k8_lb.address
@@ -39,4 +40,42 @@ resource "local_file" "ca_cert" {
   content         = module.simple_service_ca.certificate
   file_permission = "0600"
   filename        = "${path.module}/../../shared/simple_service_ca.crt"
+}
+
+resource "tls_private_key" "simple_service_client" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "tls_cert_request" "simple_service_client" {
+  private_key_pem = tls_private_key.simple_service_client.private_key_pem
+  subject {
+    common_name  = "simple-service.k8.ferlab.lan"
+    organization = "Ferlab"
+  }
+}
+
+resource "tls_locally_signed_cert" "simple_service_client" {
+  cert_request_pem   = tls_cert_request.simple_service_client.cert_request_pem
+  ca_private_key_pem = module.simple_service_ca.key
+  ca_cert_pem        = module.simple_service_ca.certificate
+
+  validity_period_hours = 87600
+  early_renewal_hours   = 720
+
+  allowed_uses = [
+    "client_auth",
+  ]
+}
+
+resource "local_file" "client_cert" {
+  content         = tls_locally_signed_cert.simple_service_client.cert_pem
+  file_permission = "0600"
+  filename        = "${path.module}/../../shared/simple_service_client.crt"
+}
+
+resource "local_file" "client_key" {
+  content         = tls_private_key.simple_service_client.private_key_pem
+  file_permission = "0600"
+  filename        = "${path.module}/../../shared/simple_service_client.key"
 }

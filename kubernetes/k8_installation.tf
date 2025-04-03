716dcc96bc7bf4aecf8a17c854c8acf56aa94b7b
Eric Vallee <eric_vallee@webificservices.com>
2025-04-03 15:05:47 -0400

    fix: INFRA-999 Changed ref for kubernetes installation

--- kubernetes/k8_installation.tf
+++ kubernetes/k8_installation.tf
@@ -26,7 +26,6 @@ module "kubernetes_installation" {
   etcd_ca_private_key = tls_private_key.k8_etcd_ca.private_key_pem
   front_proxy_ca_certificate = module.k8_certificates.front_proxy_ca_certificate
   front_proxy_ca_private_key = tls_private_key.k8_front_proxy_ca.private_key_pem
-  custom_container_repos = local.custom_container_repos
   container_registry_credentials = local.registry_credentials.credentials
   depends_on = [
     module.kubernetes_masters,

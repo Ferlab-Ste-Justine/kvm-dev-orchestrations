3909e5008dc946c6fb0ade691a2a056e284ff3ef
Eric Vallee <eric_vallee@webificservices.com>
2025-04-10 12:51:26 -0400

    feat: INFRA-982 Updated automation server reference. Updated some provider versions. Set k8 masters cpu to 2 which I previously needed to make installation work

--- automation-server-configurations/versions.tf
+++ automation-server-configurations/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     etcd = {
       source = "Ferlab-Ste-Justine/etcd"
-      version = "= 0.8.0"
+      version = "= 0.10.0"
     }
   }
   required_version = ">= 1.0.0"

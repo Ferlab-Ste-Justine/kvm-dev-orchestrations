f5a4ef6f46bfcb297dec2236eadf973af8476d8c
Sébastien Bonami <7561088+sebastienbonami@users.noreply.github.com>
2025-05-22 16:44:12 -0400

    feat: INFRA-927 enable force_destroy for bucket in smrtlink orchestration

--- smrtlink/minio_bucket.tf
+++ smrtlink/minio_bucket.tf
@@ -2,5 +2,5 @@ resource "minio_s3_bucket" "smrtlink_backup" {
   count         = local.params.smrtlink.s3_backups ? 1 : 0
   bucket        = "smrtlink-backup"
   acl           = "private"
-  force_destroy = null
+  force_destroy = true
 }

138bef6932ae22f463c1e22a934229d205b9fe13
Eric Vallee <eric_vallee@webificservices.com>
2025-07-25 23:57:27 -0400

    feat: INFRA-1220 Added capability to specify request body in patroni api script

--- shared/patroni.sh
+++ shared/patroni.sh
@@ -1,1 +1,5 @@
-curl --cacert postgres_ca.crt --cert patroni_client.crt --key patroni_client.key -X $1 "https://postgres.ferlab.lan:4443$2"
+if [ -n "$3" ]; then
+  curl --cacert postgres_ca.crt --cert patroni_client.crt --key patroni_client.key -X $1 -d "$3" "https://postgres.ferlab.lan:4443$2"
+else
+  curl --cacert postgres_ca.crt --cert patroni_client.crt --key patroni_client.key -X $1 "https://postgres.ferlab.lan:4443$2"
+fi
